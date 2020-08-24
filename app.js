66b07b0674b3ebc70f07659e20c3c956f1898013
Abdullah SEN <sen-abdullah@yandex.com>
2020-08-24 16:47:53 +0200

    Creates app structure and event handlers

---
+++ app.js
@@ -0,0 +1,49 @@
+var budgetController = (function () {
+
+})();
+
+
+var UIController = (function () {
+
+    var DOMStrings = {
+        inputType : '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMStrings.inputType).value,
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value
+            }
+
+        },
+
+        getDOMStrings : function(){
+            return DOMStrings;
+        }
+    }
+
+})();
+
+var controller = (function (budgetCtrl, UICtrl) {
+
+    var DOM  = UICtrl.getInput();
+
+    var ctrlAddItem = function () {
+        var input = UICtrl.getInput();
+        console.log(input);
+    }
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem)
+
+    document.addEventListener('keypress', function (e) {
+        if (e.keyCode === 13 || e.which === 13) {
+            ctrlAddItem();
+        }
+    })
+
+})(budgetController, UIController);
